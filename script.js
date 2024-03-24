adf69db65f0a048d0ec85b05c096e5c06daf3b1e
shorsjs <shorsjs@ukr.net>
2024-03-24 11:45:33 +0200

    321

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+function brb(number) {
+    if (number % 2 === 0)
+    {return "парне";}
+else {return "непарне";}
+}
 var numbers = [10, 20, 21, 4, 5, 6, 7, 99, 0, 12, 13];
-numbers.sort(function(a, b) {
-    return a % 2 - b % 2 || a - b;
+var парні = numbers.filter(function(number) {
+    return number % 2 === 0;
 });
+
+var непарні = numbers.filter(function(number) {
+    return number % 2 !== 0;
+});
+
+console.log("Парні числа: ", парні);
+console.log("Непарні числа: ", непарні);
