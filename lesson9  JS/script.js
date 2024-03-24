531a8cd8fe218998560e9713f0f156ba28df57f5
shorsjs <shorsjs@ukr.net>
2024-03-24 15:29:42 +0200

    9end

--- lesson9  JS/script.js
+++ lesson9  JS/script.js
@@ -1,5 +1,10 @@
-let student = {name: "Oleg",
+let student = {name:"Oleg",
 age:22,
-subjects:["higher mathematics","drawing","mechanics","Foreign Language"];}
+subjects:["higher mathematics","drawing","mechanics","Foreign Language"]}
 let keys = Object. keys(student);
 console.log (student);
+let value = Object. values(student);
+console.log(value);
+for (let key in student)
+{console.log(key + ":" + student[key]) 
+}
